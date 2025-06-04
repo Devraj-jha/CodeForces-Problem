43c8cf703b24bb30e1bf5e2501138a152c6525f0
Devraj-jha <devrajjha100806@gmail.com>
2025-06-04 07:42:45 +0530

    1

--- 800_Rated Problems/Brute Force/BeautifulYear.cpp
+++ 800_Rated Problems/Brute Force/BeautifulYear.cpp
@@ -3,7 +3,13 @@ using namespace std;
 
 int main(){
 
+  int n;
+  cin>>n;
 
+  for(int i = n; i<9000;i++)
+{
+  
+}
 
 
 
---
+++ 800_Rated Problems/Implementation /tram.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+ios::sync_with_stdio(false);
+
+int n;
+cin>>n;
+int tram[n][2];
+
+for(int i = 0; i<4; i++){
+  for(int j = 0; i< 2; j++){
+    cin>>tram[i][j];
+
+  }
+}
+
+
+
+
+
+ return 0; 
+}
