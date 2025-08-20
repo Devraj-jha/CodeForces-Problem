90eec42763f9018db3f7f71439e4fd8aea2f9056
Devraj-jha <devrajjha100806@gmail.com>
2025-08-20 11:05:09 +0530

    divisiblity_problem sol

--- 800_Rated Problems/Maths/Divisiblity_problem.cpp
+++ 800_Rated Problems/Maths/Divisiblity_problem.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int n;
-    
-    
-   
+    cin >> n;
 
-    return 0;  
+    while (n > 0) {
+        long long a, b;
+        cin >> a >> b;
+        if (a % b == 0) {
+            cout << 0 << "\n";
+        } else {
+            cout << b - (a % b) << "\n";
+        }
+        --n;
+    }
+
+    return 0;
 }
