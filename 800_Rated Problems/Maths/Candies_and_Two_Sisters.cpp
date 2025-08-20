56a5bd9343c500b4dd155699235f1f84a1c65991
Devraj-jha <devrajjha100806@gmail.com>
2025-08-20 11:57:48 +0530

    candies_and_Two_Sisters added

--- 800_Rated Problems/Maths/Candies_and_Two_Sisters.cpp
+++ 800_Rated Problems/Maths/Candies_and_Two_Sisters.cpp
@@ -6,8 +6,20 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int n;
+    cin >> n;
     
-   
+    while(n--){
+    long long n;
+    cin >> n;
+    cout << (n-1)/2 <<"\n";
 
-    return 0;  
-}
+
+     }
+     //7
+     //now think...
+     //7 = (a - 1(a)) + 1(b);
+     //the moment b > a; skip. that one..
+    // 7 / 2 = 3. = b, a = 4 
+    }
+   
