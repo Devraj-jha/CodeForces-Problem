db8d2ad27104711aa57e96d60e209049b23bc8dc
Devraj-jha <devrajjha100806@gmail.com>
2025-09-22 21:09:33 +0530

    red error fixing

--- 800_Rated Problems/Maths/Drinks.cpp
+++ 800_Rated Problems/Maths/Drinks.cpp
@@ -1,23 +1,20 @@
-#include <iostream> 
+#include <iostream>
+#include <iomanip>
+using namespace std;
 
-using namespace std; 
-
-int main(){
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int n;
-    cin >> n;  
-    double y = n;
-    double sn = 0;
+    cin >> n;
 
-    while ( n > 0 ){
+    double sn = 0;
+    for (int i = 0; i < n; i++) {
         double a;
         cin >> a;
-        sn = sn + a;
-        n--;
+        sn += a;
     }
 
-    cout << sn/(y);
-    
+    cout << fixed << setprecision(6) << sn / n;
 }
