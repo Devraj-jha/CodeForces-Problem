42155b0d388d016d98e0c0e623fbbafd86a23256
Devraj-jha <devrajjha100806@gmail.com>
2025-09-22 09:35:15 +0530

    cleaning some files

---
+++ 800_Rated Problems/Brute Force/Games.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    int cn = 0; 
+    for ( int i = 0; i < n; i ++ ){
+
+        int j,k;
+        cin >> j >> k;
+
+        if ( )
+    }
+
+   
+
+    return 0;  
+}
+
---
+++ 800_Rated Problems/greedy/I Wanna Be the Guy.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    set <int> x;
+
+    int n;
+    cin >> n;
+
+    for ( int i = 0; i < n; i++ ){
+
+        
+        
+    }
+    if ( x.size() == n){
+        cout <<"I become the guy.";
+    }else {
+        cout << "Oh, my keyboard!";
+    }
+   
+
+    return 0;  
+}
