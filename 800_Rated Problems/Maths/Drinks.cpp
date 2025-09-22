ba72d079863af7ff67ade0e9c41a8aa659c153ed
Devraj-jha <devrajjha100806@gmail.com>
2025-09-22 10:35:21 +0530

    Drinks

--- 800_Rated Problems/Maths/Drinks.cpp
+++ 800_Rated Problems/Maths/Drinks.cpp
@@ -1,8 +1,23 @@
 #include <iostream> 
+
 using namespace std; 
 
 int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    int n; 
-    cout << sizeof n; 
+    int n;
+    cin >> n;  
+    double y = n;
+    double sn = 0;
+
+    while ( n > 0 ){
+        double a;
+        cin >> a;
+        sn = sn + a;
+        n--;
+    }
+
+    cout << sn/(y);
+    
 }
