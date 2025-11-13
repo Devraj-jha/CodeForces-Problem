f7b11524cda83310253c385cbe302d264c4363ad
Devraj-jha <devrajjha100806@gmail.com>
2025-11-13 11:21:45 +0530

    test

---
+++ 1000_rated problem/constructive algorithms/DestructionoftheDandelionFields.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; //number of test cases
+
+    cin >> t;
+    
+   while (t--){
+    int n;
+    cin >> n;
+    
+
+       }
+
+    return 0;  
+}  
+
+// * n fields => ith field -> ai. (dandelions)
+//* if odd -> toggle state ( law mover)
+//* if on -> cut all subjects
+
