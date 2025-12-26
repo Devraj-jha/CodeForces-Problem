b373542bba1d13a57ce1cd6b975cc6b1a2ce57e2
Devraj-jha <devrajjha100806@gmail.com>
2025-12-26 11:39:57 +0530

    spell check

---
+++ 800_Rated Problems/Implementation /Spell_check.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string> 
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    
+    while(t--){
+      int n; 
+      cin >> n; 
+      string s; 
+      cin >> s; 
+      
+      string t = "Timur";
+      sort(s.begin(), s.end());
+      sort(t.begin(), t.end());
+
+      if(s == t) cout << "YES" <<endl;
+      else cout << "NO" <<endl;
+    }
+    return 0;  
+}  
