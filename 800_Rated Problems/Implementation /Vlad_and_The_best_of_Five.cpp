1c348fd9e90066d65136ca004baa0f8e922b4cb7
Devraj-jha <devrajjha100806@gmail.com>
2025-12-28 11:03:38 +0530

    vlad and the best of five

---
+++ 800_Rated Problems/Implementation /Vlad_and_The_best_of_Five.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string> 
+
+using namespace std;
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    
+    while(t--){ 
+        string s;
+        cin >> s;
+        int a = 0,b = 0;
+        for(int i = 0; i < 5; i++){
+            if(s[i] == 'A'){
+                ++a;
+            }else{
+                ++b;
+            }
+        }
+        if(a > b){
+            cout << "A\n";
+        }else{
+            cout << "B\n";
+        }
+    }
+
+
+    return 0;  
+}  
