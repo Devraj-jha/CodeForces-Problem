9f13648952f3f03e8bd8b68d2024bec379c361f0
Devraj-jha <devrajjha100806@gmail.com>
2025-12-28 12:31:02 +0530

    easy problem

---
+++ 800_Rated Problems/Brute Force/Easy_Problem.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while(t--) {
+        int n;
+        cin >> n;
+        cout << n - 1 << endl; // number of valid pairs
+    }
+
+    return 0;
+}
+
+//     int a, b; 
+//    if( a == n - b){
+//         cn ++ ; 
+//    } 
