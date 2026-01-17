5f9355179bb76da255a806b06eea74f31bb1c82b
Devraj-jha <devrajjha100806@gmail.com>
2026-01-17 11:58:31 +0530

    25

---
+++ 800_Rated Problems/Number_theory/Again_Twenty_Five.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;      // read input (required)
+
+    // for all n >= 2, last two digits of 5^n are always 25
+    cout << "25";
+
+    return 0;
+}
