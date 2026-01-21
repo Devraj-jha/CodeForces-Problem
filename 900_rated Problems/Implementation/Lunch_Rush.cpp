b96ff396c7adf8242dfbbf48eee1cf847d3398d7
Devraj-jha <devrajjha100806@gmail.com>
2026-01-21 11:32:37 +0530

    lunch rust

--- 900_rated Problems/Implementation/Lunch_Rush.cpp
+++ 900_rated Problems/Implementation/Lunch_Rush.cpp
@@ -1,37 +1,36 @@
 #include <iostream>
-#include <utility>
 #include <vector>
-
+#include <utility>
+#include <climits>
 
 using namespace std;
 
-int main(){
-
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    long long  n,k; 
+    long long n, k; 
     cin >> n >> k; 
-    vector<pair<long long, long long > > restaurants(n);
-
+    vector<pair<long long, long long>> restaurants(n);
 
     for (int i = 0; i < n; i++) {
-    long long f, t;
-    cin >> f >> t;                // read fi and ti
-    restaurants[i] = {f, t};      // assign them to the pair
-}
-    long long  max_joy = 0; 
-    long long  joy = 0; 
-for(int i = 0; i < n; i ++){
-    if(restaurants[i].second > k){
-        joy = restaurants[i].first - (restaurants[i].second - k);
-        
-    }else{
-        joy = restaurants[i].first; 
-
+        long long f, t;
+        cin >> f >> t;
+        restaurants[i] = {f, t};
     }
-   max_joy =  max(max_joy,joy);
-}
-cout << max_joy << "\n";
-    
+
+    long long max_joy = LLONG_MIN; // important for negative joys
+    for (int i = 0; i < n; i++) {
+        long long joy;
+        if (restaurants[i].second > k) {
+            joy = restaurants[i].first - (restaurants[i].second - k);
+        } else {
+            joy = restaurants[i].first;
+        }
+        max_joy = max(max_joy, joy);
+    }
+
+    cout << max_joy << "\n";
+
+    return 0;
 }
