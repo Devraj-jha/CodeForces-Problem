2eb6fcdd8fb3c93cf7a1d77d81f7b32126f53b04
Devraj-jha <devrajjha100806@gmail.com>
2026-01-29 21:57:10 +0530

    medium_number

---
+++ 800_Rated Problems/sortings/Medium_Number.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    
+    while(t--){
+
+        vector<int> v(3);
+
+         for(int i = 0; i < 3; i++) {
+        cin >> v[i];       
+     }
+     sort(v.begin(), v.end());
+        
+     cout << v[1] << endl;
+    }
+
+
+    return 0;  
+}  
