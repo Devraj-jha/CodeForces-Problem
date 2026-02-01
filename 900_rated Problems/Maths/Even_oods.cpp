85903184147a434cd3a3bf0eb9ce7adbca59a551
Devraj-jha <devrajjha100806@gmail.com>
2026-02-01 21:00:00 +0530

    even odd

---
+++ 900_rated Problems/Maths/Even_oods.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n,k; 
+    int cn = 0;
+
+    cin >> n >> k; 
+    long long odd_count = (n + 1)/ 2; 
+
+    if(k <= odd_count){
+        cout << 2 * k -1;
+
+    }else {
+        cout << 2 * (k - odd_count);
+    }
+    return 0;  
+}  
