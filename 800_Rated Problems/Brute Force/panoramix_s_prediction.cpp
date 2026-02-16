032af1c7e540d60f35a8b70c665a7d8dba514cbf
Devraj-jha <devrajjha100806@gmail.com>
2026-02-16 21:27:35 +0530

    panoramix

---
+++ 800_Rated Problems/Brute Force/panoramix_s_prediction.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    if (n == 2) return true;
+    if (n % 2 == 0) return false;
+
+    for (int i = 3; i * i <= n; i += 2) {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = n + 1; i <= m; i++) {
+        if (isPrime(i)) {
+            if (i == m)
+                cout << "YES";
+            else
+                cout << "NO";
+            return 0;
+        }
+    }
+
+    cout << "NO";
+    return 0;
+}
