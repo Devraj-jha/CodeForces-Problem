91a678eae5d2ffc27ebce38c90d1629820a9031d
Devraj-jha <devrajjha100806@gmail.com>
2026-03-23 17:14:52 +0530

    candies

---
+++ 900_rated Problems/Brute_Force/Candles.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while(t--) {
+        int n;
+        cin >> n;
+
+        for(int k = 2; k < 30; k++) {
+            int sum = (1 << k) - 1;  // 2^k - 1
+
+            if(n % sum == 0) {
+                cout << n / sum << endl;
+                break;
+            }
+        }
+    }
+}
