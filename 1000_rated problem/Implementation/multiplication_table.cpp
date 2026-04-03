1bc448816770c28f145e952f082a49a43811d038
Devraj-jha <devrajjha100806@gmail.com>
2026-04-03 22:43:31 +0530

    mul

---
+++ 1000_rated problem/Implementation/multiplication_table.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n, x;
+    cin >> n >> x;
+
+    int count = 0;
+
+    for(int i = 1; i <= n; i++){
+        if(x % i == 0){
+            int j = x / i;
+            if(j <= n){
+                count++;
+            }
+        }
+    }
+
+    cout << count;
+    return 0;
+}
