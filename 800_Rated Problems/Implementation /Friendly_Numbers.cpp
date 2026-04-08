590a01f5423250a3294d769c09aa870ea5e62265
Devraj-jha <devrajjha100806@gmail.com>
2026-04-08 23:06:28 +0530

    hard

---
+++ 800_Rated Problems/Implementation /Friendly_Numbers.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int digitSum(long long n) {
+    int sum = 0;
+    while(n) {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while(t--) {
+        long long x;
+        cin >> x;
+
+        int count = 0;
+
+        for(long long y = x; y <= x + 90; y++) {
+            if(y - digitSum(y) == x) {
+                count++;
+            }
+        }
+
+        cout << count << '\n';
+    }
+
+    return 0;
+}
