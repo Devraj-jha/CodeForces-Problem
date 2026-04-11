e2dd320d7e6fd22d8c652b5d4f4c11565ebe36dd
Devraj-jha <devrajjha100806@gmail.com>
2026-04-11 23:56:05 +0530

    xor

---
+++ 800_Rated Problems/Implementation /odd_one_out.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+
+
+using namespace std;
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    
+    while(t--){
+           int a,b,c; 
+    cin >> a >> b >> c; 
+
+    cout << (a ^ b ^ c) << endl;
+
+
+    }
+
+
+    return 0;  
+}  
