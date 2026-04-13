356ff2ce671890067063fd9b9cf74a9de02aa084
Devraj-jha <devrajjha100806@gmail.com>
2026-04-13 22:20:30 +0530

    present

--- 800_Rated Problems/Implementation /Presents.cpp
+++ 800_Rated Problems/Implementation /Presents.cpp
@@ -8,13 +8,11 @@ int main() {
 
     vector<int> p(n + 1), giver(n + 1);
 
-    // Read who each friend gave a gift to
     for (int i = 1; i <= n; i++) {
         cin >> p[i];
         giver[p[i]] = i;  // inverse mapping
     }
 
-    // Output who gave a gift to each friend
     for (int i = 1; i <= n; i++) {
         cout << giver[i] << " ";
     }
