ea324edc546c02cb6dafb2bd87ec99e703055242
Devraj-jha <devrajjha100806@gmail.com>
2026-04-20 22:05:19 +0530

    even

--- 800_Rated Problems/greedy/EvenArray.cpp
+++ 800_Rated Problems/greedy/EvenArray.cpp
@@ -1,25 +1,35 @@
+
 #include <iostream>
 #include <vector>
-#include <algorithm>
-
 using namespace std;
 
-int main(){
-
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int t;
     cin >> t;
-    
-    
-    while(t--){
-        int n; 
-        cin >> n; 
-        vector <int> v(n);
-       
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        int evenMismatch = 0, oddMismatch = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 != a[i] % 2) {
+                if (i % 2 == 0) evenMismatch++;
+                else oddMismatch++;
+            }
+        }
+
+        if (evenMismatch != oddMismatch) {
+            cout << -1 << "\n";
+        } else {
+            cout << evenMismatch << "\n";
+        }
     }
 
-
-    return 0;  
-}  
+    return 0;
+}
