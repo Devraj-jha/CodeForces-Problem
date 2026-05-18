08d788836e26d8d19c5539c7073bceaf3a673802
devraj-jha <devrajjha100806@gmail.com>
2026-05-18 20:19:29 +0530

    Resolved merge conflict

--- cppSkeletonTestCases.cpp
+++ cppSkeletonTestCases.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main(){
@@ -10,9 +11,16 @@ int main(){
     cin >> t;
     
     while(t--){
-        
 
+    int n;
+    cin >> n;
+
+    vector<int> v(n);      
+    for(int i = 0; i < n; i++) {
+        cin >> v[i];       
+    }
     }
 
-    return 0;  
-}  
+    return 0;
+ }
+
---
+++ test.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n,m,a,b
+    ; 
+    cin >> n >> m >> a >>b; 
+
+
+    int cn = 0; 
+
+    
+    // one side => a rubles
+    //speical ticket for -> m rides
+    // costs b rubbel
+    // n times subway needs
+
+
+
+
+    return 0;  
+}  
