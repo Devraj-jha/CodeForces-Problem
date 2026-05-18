46e3cd3b97d5860fb4ee4e18528cbd6edc31a146
Devraj-jha <devrajjha100806@gmail.com>
2026-05-18 12:59:42 +0530

    geometry

---
+++ 800_Rated Problems/Geometry/Fashionablee.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    
+    while(t--){
+        long long n; 
+        cin >> n; 
+        if(n % 4 == 0){
+            cout << "YES\n";
+        }else {
+            cout << "NO\n";
+        }
+
+    }
+
+    return 0;  
+}  
