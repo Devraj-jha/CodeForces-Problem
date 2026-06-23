894124505f6dbd57c49512c672cf87fd4193fc9e
Devraj-jha <devrajjha100806@gmail.com>
2026-06-23 18:48:06 +0530

    math

---
+++ 800_Rated Problems/Maths/Two_Elevators.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        int t1 = abs(a - 1);
+        int t2 = abs(b - c) + abs(c - 1);
+
+        if (t1 < t2)
+            cout << 1 << "\n";
+        else if (t2 < t1)
+            cout << 2 << "\n";
+        else
+            cout << 3 << "\n";
+    }
+
+    return 0;
+}
