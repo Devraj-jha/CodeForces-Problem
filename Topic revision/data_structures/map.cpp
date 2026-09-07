76f829b2ff9f7c94cf45cdeefea5beb22e4bfbdb
Devraj-jha <devrajjha100806@gmail.com>
2026-09-07 05:34:01 +0530

    map

---
+++ Topic revision/data_structures/map.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+#include <map>
+#include <unordered_map>
+using namespace std;
+
+int main(){
+
+    map <string, int> ages; 
+
+    ages["alice"] = 34; 
+    // key - value
+    // when we need sorted keys we use it 
+
+
+    unordered_map < char , int> num; 
+    
+    // key value pair unsorted fast look up ;
+}
