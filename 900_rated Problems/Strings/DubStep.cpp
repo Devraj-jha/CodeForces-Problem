db3bc323341821db8845ba257126093c48c4c8ad
Devraj-jha <devrajjha100806@gmail.com>
2026-09-18 08:40:20 +0530

    string thing

--- 900_rated Problems/Strings/DubStep.cpp
+++ 900_rated Problems/Strings/DubStep.cpp
@@ -1,16 +1,39 @@
-
 #include <iostream>
-#include <string> 
+#include <string>
 using namespace std;
 
-int main(){
+int main() {
 
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    string n; 
-    cin >> n; 
-    
+    string n;
+    cin >> n;
 
-    return 0;  
-}  
+    string ans;
+
+    for (int i = 0; i < n.length(); i++) {
+
+        if (i + 2 < n.length() &&
+            n[i] == 'W' &&
+            n[i + 1] == 'U' &&
+            n[i + 2] == 'B') {
+
+            ans += ' ';
+            i += 2;
+
+        } else {
+            ans += n[i];
+        }
+    }
+
+    if (!ans.empty() && ans[0] == ' ')
+        ans.erase(0, 1);
+
+    if (!ans.empty() && ans.back() == ' ')
+        ans.pop_back();
+
+    cout << ans << '\n';
+
+    return 0;
+}
