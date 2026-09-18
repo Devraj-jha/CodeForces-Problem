479cd4d731432450a81fdd781460bb3c466ae91a
Devraj-jha <devrajjha100806@gmail.com>
2026-09-18 08:20:43 +0530

    docs: auto-update README & structure [283 solutions]

--- test.cpp
+++ test.cpp
@@ -1,26 +1,5 @@
-
 #include <iostream>
-#include <string>
-using namespace std;
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int n;
-    cin >> n;
-    string s;
-    cin >> s;
-
-    string ans = "";
-
-    for (int i = n - 1; i >= 0; i--) {
-        int pos = ans.size() / 2;   
-        ans.insert(ans.begin() + pos, s[i]);
-    }
-
-    cout << ans << "\n";
-    return 0;
-}
+ using namespace std; int main() { cout << "Hello world" ; }
 
 // 0 1 2 3 4 5 
 
